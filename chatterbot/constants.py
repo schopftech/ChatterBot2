6d0f115354e1d4adbda694f27e1b4089d84032a3
Marvin <marvin@schopf.biz>
2021-01-09 17:04:45 +0100

    Update constants.py

--- chatterbot/constants.py
+++ chatterbot/constants.py
@@ -7,6 +7,8 @@ The maximum length of characters that the text of a statement can contain.
 The number 255 is used because that is the maximum length of a char field
 in most databases. This value should be enforced on a per-model basis by
 the data model for each storage adapter.
+
+Changed this to 1024 so Chatterbot is compatible with Ubuntu corpus.
 '''
 STATEMENT_TEXT_MAX_LENGTH = 1024
 
